2e964bc9636e10826faca4028f6cebfe72b7a204
Shivam Jawanjal <shivamjpatil2007@gmail.com>
2025-02-17 15:51:53 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const connectToMongo = require('./db');
 const express = require('express');
 var cors = require('cors');
 
-
 connectToMongo();
 var app = express();
 
-app.use(cors());
+// Enable CORS for localhost:3000
+const corsOptions = {
+    origin: 'http://localhost:3000',  // Allow requests from this domain
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allow specific methods
+    credentials: true,  // If you need to send cookies or authentication headers
+};
+
+app.use(cors(corsOptions));  // Apply CORS middleware with options
+
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
@@ -16,8 +23,13 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/class', require('./routes/class'));
 app.use('/api/microproject', require('./routes/microproject'));
 app.use('/api/attendance', require('./routes/attendance'));
-app.use('/api/upload', require('./routes/uploadRoutes'));  // Updated this line
+app.use('/api/upload', require('./routes/uploadRoutes'));  
 app.use('/api/uploadurl', require('./routes/googlesheet'));
+
+// Root route
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
